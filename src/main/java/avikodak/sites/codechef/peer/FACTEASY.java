aff2e05d25e37e2eca420f7d05c4dec19e3c3182
Avinash Kodakandla <avinashk@vaycayhero.com>
2015-09-17 17:26:52 +0530

    180 problem YAY!!! codechef

--- src/main/java/avikodak/sites/codechef/peer/FACTEASY.java
+++ src/main/java/avikodak/sites/codechef/peer/FACTEASY.java
@@ -1,13 +1,20 @@
+/****************************************************************************************************************************************************
+ *  File Name                   : FACTEASY.java
+ *  Created on                  : 17-Sep-2015 :: 5:23:22 pm
+ *  Author                      : avikodak
+ *  Testing Status              : Tested
+ *  URL                         : https://www.codechef.com/problems/FACTEASY
+ ****************************************************************************************************************************************************/
+
+package avikodak.sites.codechef.peer;
+
 import java.math.BigInteger;
 import java.util.Scanner;
 
-/****************************************************************************************************************************************************
- *  File Name                   : Main.java
- *  Author                      : avikodak
- ****************************************************************************************************************************************************/
+//Tested
+public class FACTEASY {
 
-public class Main {
-	
+	//Tested
 	public static void calculateFactorial(BigInteger userInput){
 		BigInteger result = BigInteger.ONE;
 		while(userInput.compareTo(BigInteger.ONE) != 0){
@@ -17,6 +24,7 @@ public class Main {
 		System.out.println(result);
 	}
 
+	//Tested
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Integer testCases = in.nextInt();
